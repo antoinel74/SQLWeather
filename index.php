3f28c307ab3c6070f833fee2da09624ac95ceb90
antoinel74 <antoine.lansman@gmail.com>
2023-11-09 10:50:35 +0100

    fetching and html table done

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <?php
-echo '<h1>Test</h1>';
-
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=weatherapp', 'root', 'root');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = $pdo->query('SELECT * FROM météo');
+    $data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    print_r($data);
+
+    echo '<table border="1">';
+    echo '<tr>';
+    foreach ($data[0] as $key => $value) {
+        echo '<th>' . $key . '</th>';
+    }
+    echo '</tr>';
+
+    foreach ($data as $row) {
+        echo '<tr>';
+        foreach ($row as $value) {
+            echo '<td>' . $value . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
 } catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
+    die('Erreur PDO: ' . $e->getMessage());
 }
